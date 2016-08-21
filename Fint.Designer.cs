3a79211ab4347a7a0d38bdbe520768ed6fc49624
valle1235 <vallen0002@gmail.com>
2016-08-21 19:05:39 +0200

    Add files via upload

---
+++ Fint.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace Finherrans
+{
+    partial class Fint
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Fint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Finherrans.Properties.Resources.corn_field_4_a;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(644, 411);
+            this.DoubleBuffered = true;
+            this.Name = "Fint";
+            this.Text = "Finherrans";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Fint_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Fint.cs
@@ -0,0 +1,57 @@
+﻿#define My_Debug
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finherrans
+{
+    public partial class Fint : Form
+    {
+        int _cursX = 0;
+        int _cursY = 0;
+
+        public Fint()
+        {
+            InitializeComponent();
+        }
+
+        private void GameLoop(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timerGameLoop_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics dc = e.Graphics;
+#if My_Debug
+
+            TextFormatFlags flags = TextFormatFlags.Left | TextFormatFlags.EndEllipsis;
+            Font _font = new System.Drawing.Font("Stencil", 12, FontStyle.Regular);
+            TextRenderer.DrawText(dc, "X=" + _cursX.ToString() + ":" + "Y=" + _cursY.ToString(), _font,
+                new Rectangle(0, 0, 120, 20), SystemColors.ControlText, flags);
+
+#endif
+
+            base.OnPaint(e);
+        }
+
+        private void Fint_MouseMove(object sender, MouseEventArgs e)
+        {
+            _cursX = e.X;
+            _cursY = e.Y;
+
+            this.Refresh();
+        }
+    }
+}
